3a7a18c65405bd24a6cb1f555d1d108ae575bde9
AlexWei061 <aty.wei@foxmail.com>
2025-01-21 10:05:34 +0800

    test sample

--- rust_code.rs
+++ rust_code.rs
@@ -1,25 +1,22 @@
 use std::io;
-
+const MAXN: usize = 3030;
 fn main() {
     let mut n = String::new();
-    io::stdin().read_line(&mut n).expect("Error reading input");
-    let n: i32 = n.trim().parse().expect("Invalid input");
-    let mut a = vec![0; 3030];
-
+    io::stdin().read_line(&mut n).unwrap();
+    let n: u32 = n.trim().parse().unwrap();
+    let mut a = vec![0; MAXN];
     for i in 1..=n {
-        let mut num = String::new();
-        io::stdin().read_line(&mut num).expect("Error reading input");
-        let x: i32 = num.trim().parse().expect("Invalid input");
-        a[i as usize] = x;
+        let mut temp = String::new();
+        io::stdin().read_line(&mut temp).unwrap();
+        a[i] = temp.trim().parse::<u32>().unwrap();
     }
-
     for i in 2..=n {
         a[1] &= a[i];
     }
-
     if a[1] == 0 {
         println!("Yes");
     } else {
         println!("No");
     }
+}
 }
