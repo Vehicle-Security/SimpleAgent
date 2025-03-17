daec316a1403fc75b2b29799ea31c44979e08a7d
AlexWei061 <aty.wei@foxmail.com>
2025-03-17 22:31:20 +0800

    organize code

--- test_code/output/example.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-
-fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let n: i32 = input.trim().parse().expect("Please type a number!");
-    println!("n = {}", n);
-}
